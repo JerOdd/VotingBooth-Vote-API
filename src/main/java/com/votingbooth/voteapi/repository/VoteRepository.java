b7568329100519ff9119102f27b1855afcae8cf4
Jeremy Oddos <jeremyo@voodoo.io>
2024-07-09 17:04:20 +0200

    feat: inject redis credential from env var

--- src/main/java/com/votingbooth/voteapi/repository/VoteRepository.java
+++ src/main/java/com/votingbooth/voteapi/repository/VoteRepository.java
@@ -3,15 +3,30 @@ package com.votingbooth.voteapi.repository;
 import com.votingbooth.voteapi.model.LawStatus;
 import com.votingbooth.voteapi.model.Vote;
 import com.votingbooth.voteapi.model.VoteResult;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import redis.clients.jedis.JedisPooled;
 import redis.clients.jedis.resps.Tuple;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
 @Repository
 public class VoteRepository {
-    JedisPooled jedis = new JedisPooled("localhost", 6379);
+
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+    @Value("${REDIS_PORT}")
+    private int redisPort;
+
+    private JedisPooled jedis;
+
+    @PostConstruct
+    public void createRedisPool() {
+        jedis = new JedisPooled(redisHost, redisPort);
+    }
 
     public boolean isLawOpen(String lawId) {
         return getLawStatus(lawId) == LawStatus.OPEN;
